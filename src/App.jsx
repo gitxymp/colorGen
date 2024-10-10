e3457676debbea32b9a8d04c31922f055df4e966
gitxymp <147992942+gitxymp@users.noreply.github.com>
2024-10-11 00:13:25 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import RandomColor from "./Components/random-color";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomColor />
+    </div>
+  );
+}
+
+export default App;
